eddcd1416a3bb37665a8594d7d123a78b53f62a0
Michael Crumm <mike.crumm@groundctrl.com>
2014-07-21 00:05:58 -0700

    Give RandomBot a little more brains

--- src/Bot/RandomBot.hh
+++ src/Bot/RandomBot.hh
@@ -5,14 +5,20 @@ namespace Hackdinium\Bot;
 use Hackdinium\Aim;
 use Hackdinium\Compass;
 use Hackdinium\Input;
-use Hackdinium\Tile\Wall;
+use Hackdinium\Tile;
 
 class RandomBot extends Robot {
     use Item\Compass;
 
     public function move(Input $input) : Aim {
         $dir = $this->first(
-            (Aim $dir) ==> !is_a($input->game->board->at($input->hero->pos->to($dir)), Wall::class),
+		(Aim $dir) ==> {
+		    $move = $input->game->board->at($input->hero->pos->to($dir)); 
+		    return !is_a($move, Tile\Wall::class)
+			    && !is_a($move, Tile\Tavern::class)
+			    && !is_a($move, Tile\MineTile::class)
+		    ;
+		},
             $this->shuffle($this->aim->toVector())
         );
 

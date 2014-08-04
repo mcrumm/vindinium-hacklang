155ae9951c9c55d6d02535c4611cac2415a713c0
Michael Crumm <mike.crumm@groundctrl.com>
2014-08-04 16:04:20 -0700

    Added a helpful functions namespace

--- src/Bot/Item/Compass.hh
+++ src/Bot/Item/Compass.hh
@@ -3,6 +3,7 @@ namespace Hackdinium\Bot\Item;
 
 use Hackdinium\Aim;
 use Hackdinium\Bot\Robot;
+use Hackdinium\f;
 
 trait Compass {
     require extends Robot;
@@ -17,14 +18,15 @@ trait Compass {
     }
 
     public function find(Vector<Aim> $v, (function(Aim): bool) $f, Aim $none) : Aim {
-        return \Hackdinium\getOrElse($this->reduce($v, $f)->get(0), $none);
+        return f\getOrElse(f\reduce($v->toImmVector(), $f)->get(0), $none);
     }
 
     public function first((function(Aim): bool) $f, ?Vector<Aim> $v = null, ?Aim $none = null) : Aim {
-        return $this->find(\Hackdinium\getOrElse($v, $this->aim->toVector()), $f, \Hackdinium\getOrElse($none, Aim::NORTH()));
+        $v = null === $v ? $this->aim->toImmVector() : $v->toImmVector();
+        return f\getOrElse(f\find($v, $f), f\getOrElse($none, Aim::STAY()));
     }
 
     private function reduce<T>(Vector<T> $v, (function(T) : bool) $f) : Vector<T> {
-        return $v->filter($f);
+        return f\reduce($v->toImmVector(), $f)->toVector();
     }
 }
